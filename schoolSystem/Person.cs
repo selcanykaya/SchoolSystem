01cd90fc68b62fea0d4ba38363fcdb7c8da6f831
Administrator <Administrator@DESKTOP-ITO9GJ1>
2025-05-24 01:02:19 +0300

    Add project files.

---
+++ schoolSystem/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace schoolSystem
+{
+    public class Person
+    {   
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public void Info()
+        {
+            Console.WriteLine("Name: " + Name + " Surname: " + Surname + " Date of Birth: " + DateOfBirth);
+        }
+    }
+}
---
+++ schoolSystem/Program.cs
@@ -0,0 +1,41 @@
+﻿using schoolSystem;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Person student1 = new Person
+        {
+            Name = "Selena",
+            Surname = "Gomez",
+            DateOfBirth = new DateTime(1992, 7, 22)
+        };
+
+        Person student2 = new Person
+        {
+            Name = "Justin",
+            Surname = "Bieber",
+            DateOfBirth = new DateTime(1994, 3, 1)
+        };
+
+        Person teacher1 = new Person
+        {
+            Name = "Demi",
+            Surname = "Lovato",
+            DateOfBirth = new DateTime(1992, 8, 20)
+        };
+
+        Person teacher2 = new Person
+        {
+            Name = "Miley",
+            Surname = "Cyrus",
+            DateOfBirth = new DateTime(1992, 11, 23)
+        };
+
+        student1.Info();
+        student2.Info();
+        teacher1.Info();
+        teacher2.Info();
+
+    }
+}
